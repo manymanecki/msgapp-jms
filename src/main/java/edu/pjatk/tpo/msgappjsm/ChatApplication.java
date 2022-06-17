2db5827c6236e53d8ec55ffe2fcca33e8ecbddae
many <jakub.pawlowski00@gmail.com>
2022-06-17 23:46:54 +0200

    Modifications of chat window (17.06.2022-23:46)

--- src/main/java/edu/pjatk/tpo/msgappjsm/ChatApplication.java
+++ src/main/java/edu/pjatk/tpo/msgappjsm/ChatApplication.java
@@ -3,7 +3,9 @@ package edu.pjatk.tpo.msgappjsm;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,12 +14,23 @@ public class ChatApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("chat-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("msgapp-jsm - Messaging Application");
+        scene.getStylesheets().setAll(
+                getClass().getResource("chat-view.css").toExternalForm()
+        );
+
+        stage.setTitle("msgapp-jsm - Instant Messaging Application");
         stage.setScene(scene);
         stage.sizeToScene();
+
+        // Transparent background to make rounded corners work
+        scene.setFill(Color.TRANSPARENT);
+        //stage.initStyle(StageStyle.TRANSPARENT);
+
+        // Minimal window size set as 800x600
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
+
         stage.show();
-        stage.setMinWidth(stage.getWidth());
-        stage.setMinHeight(stage.getHeight());
     }
 
     public static void main(String[] args) {

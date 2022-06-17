60f08fdd3c7ffaa0b26f44f5b968bab142e7b048
many <jakub.pawlowski00@gmail.com>
2022-06-17 19:46:05 +0200

    First update to the chat window

---
+++ src/main/java/edu/pjatk/tpo/msgappjsm/ChatApplication.java
@@ -0,0 +1,26 @@
+package edu.pjatk.tpo.msgappjsm;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ChatApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("chat-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("msgapp-jsm - Messaging Application");
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
+        stage.setMinWidth(stage.getWidth());
+        stage.setMinHeight(stage.getHeight());
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}

2c7bf8dbfbc0a21ad7d57d7ad6b48d68fb6d4d72
many <jakub.pawlowski00@gmail.com>
2022-06-18 02:48:35 +0200

    Framework for login screen + renaming of classes

---
+++ src/main/java/edu/pjatk/tpo/msgappjsm/LoginController.java
@@ -0,0 +1,7 @@
+package edu.pjatk.tpo.msgappjsm;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+}
--- src/main/java/edu/pjatk/tpo/msgappjsm/MainApplication.java
+++ src/main/java/edu/pjatk/tpo/msgappjsm/MainApplication.java
@@ -5,17 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class ChatApplication extends Application {
+public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ChatApplication.class.getResource("chat-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("chat.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         scene.getStylesheets().setAll(
-                getClass().getResource("chat-view.css").toExternalForm()
+                getClass().getResource("chat.css").toExternalForm()
         );
 
         stage.setTitle("msgapp-jsm - Instant Messaging Application");
